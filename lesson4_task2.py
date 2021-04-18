dce96a4a0463be7b5482270f527bd6151064b455
xaMMax <petrolijuice@gmail.com>
2021-04-18 15:49:31 +0300

    Create second homework for lesson 4. ver 1.0

---
+++ lesson4_task2.py
@@ -0,0 +1,6 @@
+name = input('Hello, please input your name ') #input name
+age = int(input('Hello, please input your age ')) #input age
+if 0 < age < 101: #if statement for correct age
+    print('Hello ' + name + ', on your next birthday you’ll be ' + str(int(age + 1)) + ' years!') #print result
+else:
+    print('Who are you alien? -_-' + 'on your next birthday you’ll be ' + str(int(age + 1)) + ' years!') #print result if age is incredible
