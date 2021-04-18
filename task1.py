4c3baa07d6d6e3ed361c20b86b1dc1bed6d425dc
xaMMax <petrolijuice@gmail.com>
2021-04-18 15:23:55 +0300

    ''

--- task1.py
+++
@@ -1,5 +0,0 @@
-yourStr = input('Please type your string here ')            # input sample string
-if len(yourStr) >= 2:                                       # use function len for compare and go to next
-    expString = yourStr[:2] + yourStr[-2:]                  # use indexing for doing lesson
-    print('your string is ' + yourStr, 'and expected result is ' + expString) #print our condidions on screen
-else: print('empty string')                                 # if yourStr len less than 2
