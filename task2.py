29478bb804b03a8066ce45f185002366018c68fa
xaMMax <petrolijuice@gmail.com>
2021-04-18 13:16:56 +0300

    task2 ver 1.1 'добавил 3 вариант с использованием тернарного оператора'

--- task2.py
+++ task2.py
@@ -7,13 +7,19 @@ else:
     print('ERROR: you don`t use numbers or incorrect length of number') #returns error message
 """
 #2 variant
-
+'''
 phoneNum = input('Type your phone number ')  # create a variable
 if phoneNum.isdigit():  # if our variable is digital
     if len(phoneNum) == 10:  # if our variable is same length
-       print(phoneNum)  # print phone number
+       print('your phone number is: ' + phoneNum)  # print phone number
     else:
             print('ERROR: your phone length is incorrect')  # print error message with error type
 else:
     print('ERROR: you don`t use numbers')  # print error message with error type
+'''
+#3 variant 'ternar operator'
 
+phoneNum = input('''Type your phone number 
+(note: phone number must contain 10 digits, without alphabet symbols)
+:''')  # create a variable
+print('your phone number is: ' + phoneNum) if type(phoneNum == int) and len(phoneNum) == 10 else print('error: phone number is incorrect') #
