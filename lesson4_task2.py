aa8efb96f83a50ab0a1e4a5b090663835c8b66ea
xaMMax <petrolijuice@gmail.com>
2021-04-19 19:05:35 +0300

    some minimal changes

--- lesson4_task2.py
+++ lesson4_task2.py
@@ -3,4 +3,4 @@ age = int(input('Hello, please input your age ')) #input age
 if 0 < age < 101: #if statement for correct age
     print('Hello ' + name + ', on your next birthday you’ll be ' + str(int(age + 1)) + ' years!') #print result
 else:
-    print('Who are you alien? -_-' + 'on your next birthday you’ll be ' + str(int(age + 1)) + ' years!') #print result if age is incredible
+    print('Who are you alien? -_-' + ' on your next birthday you’ll be ' + str(int(age + 1)) + ' years!') #print result if age is incredible
--- lesson4_task3.py
+++ lesson4_task3.py
@@ -7,4 +7,4 @@ else:
     while i < 5:    # use loop for generated 5 prints
         result1 = ''.join((random.sample(yourString, len(yourString))))    # use join and random method for do random string
         i = i + 1   # use i + 1 for good loop
-        print(" Random generated string x5: ", result1)  # print new string 5 times
+        print(" Random generated string: ", result1)  # print new string 5 times
