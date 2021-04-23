9b9390a79a629b83e8d0e0cb714463598eeacc61
xaMMax <petrolijuice@gmail.com>
2021-04-23 19:27:31 +0300

    Исправил что понимаю, остальное на занятии мож получится разобрать, 3 задание і- й элемент.

--- Lesson5/lesson5_task1.py
+++ Lesson5/lesson5_task1.py
@@ -3,10 +3,9 @@ import random  # import random module
 listNum = []  # create list
 i = 0  # i for use in while loop
 while i < 10:  # while loop
-    randNum = round(random.random(), 2) # generate random float again and again with round of 2 decimal places
+    randNum = random.randint(1, 10) # generate random float again and again with round of 2 decimal places
     #  print(randNum)
-    listNum.extend([randNum])  # extend our list by new random number
+    listNum.append(randNum)  # extend our list by new random number
     #  print(listNum)
     i += 1  # our loop i
 print('Our list of random numbers is ', listNum, '\nand the largest number in this list = ', max(listNum))  # use print for users
-
--- Lesson5/lesson5_task2.py
+++ Lesson5/lesson5_task2.py
@@ -6,11 +6,10 @@ i = 0  # i for use in while loop
 while i < 10:  # while loop
     randNum0 = random.randint(1, 10)  # generate random integer 10 times for first list
     randNum1 = random.randint(1, 10)  # generate random integer 10 times for second list
-    list1.extend([randNum0])  # extend our first list by new random integer
-    list2.extend([randNum1])  # extend our second list by new random integer
+    list1.append(randNum0)  # extend our first list by new random integer
+    list2.append(randNum1)  # extend our second list by new random integer
     i += 1  # our loop i
-print('We have list1, and list2 with random integers\n', list1, '\n', list2)
-list1.extend(list2)  # make 2 lists in one, extend first list by second
-print('Our new list of list1 and list2\n', list1)  # print our new list for watch what we do
-list1 = set(list1)  # use set for get exclusive numbers in our list
+list1 = set(list1) | set(list2)  # use union sets
 print('Our list of with exclusive numbers is\n', list(list1))  # use list for carry out lesson task
+
+
