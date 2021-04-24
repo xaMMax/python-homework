e5a1c69b746ab8bf5aa74543ae08a1ed9cc92b96
xaMMax <petrolijuice@gmail.com>
2021-04-24 23:03:41 +0300

    new homework

---
+++ lesson6/lesson6_task1.py
@@ -0,0 +1,12 @@
+i = 0
+string1 = {}
+m = 'abcda'
+while i <= 5:
+    string1 = dict.fromkeys(m, [i])
+    i += 1
+    print(string1)
+
+print(string1)
+# for i in string1:
+#     string1[] = i
+#     print(string1)
