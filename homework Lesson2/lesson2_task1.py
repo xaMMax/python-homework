9079e7ed2b5a8b3351e048f476c393d073e5b37a
xaMMax <petrolijuice@gmail.com>
2021-04-24 16:28:19 +0300

    new homework

--- homework Lesson2/lesson2_task1.py
+++
@@ -1,3 +0,0 @@
-name = 'maks'
-day = 'tuesday'
-print(f'Good day {name.title()}! {day.title()} is a perfect day to learn some python.')
--- lesson4/lesson4_task2.py
+++
@@ -1,6 +0,0 @@
-name = input('Hello, please input your name: ')  # input name
-age = int(input('and, please input your age: '))  # input age
-if 0 < age < 101:  # if statement for correct age
-    print('Greetings ' + name + ', next birthday you will be ' + str(int(age + 1)) + ' years!')  # print result
-else:
-    print('Who are you alien? -_-' + ' on your next birthday you’ll be ' + str(int(age + 1)) + ' years! -_-')  # print result if age is incredible
