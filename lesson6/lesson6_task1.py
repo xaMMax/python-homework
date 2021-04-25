21ec5da6c53d21f7440f08cf58c35149169c929a
xaMMax <petrolijuice@gmail.com>
2021-04-25 10:11:03 +0300

    task 1 ver 1.0

--- lesson6/lesson6_task1.py
+++ lesson6/lesson6_task1.py
@@ -1,12 +1,9 @@
+m = input('Input text here: ')  # create string
+m = m.strip(' ')  # delete spaces in start and finish of the string
+m = m.split(' ')  # use for create list
+d = dict.fromkeys(m)  # create dict from our list with keys 'm' and values 'None'
 i = 0
-string1 = {}
-m = 'abcda'
-while i <= 5:
-    string1 = dict.fromkeys(m, [i])
+for key in d:  # for len d.keys
+    d[key] = [i]  # create values for our dictionary
     i += 1
-    print(string1)
-
-print(string1)
-# for i in string1:
-#     string1[] = i
-#     print(string1)
+print('Your dictionary is:\n', d)  # print dictionary with unique words and values of occurrences
