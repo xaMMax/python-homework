cd2259e2d837b2766124c7df525cf552f26b3236
xaMMax <petrolijuice@gmail.com>
2021-04-27 06:43:24 +0300

    task 1, 3 with list, and dict comprehension

--- lesson6/lesson6_task1.py
+++ lesson6/lesson6_task1.py
@@ -2,3 +2,4 @@ m = input('Input text here: ')  # create string
 m = m.split()  # use split for crate some list
 count = {i: m.count(i) for i in m if i != ''}  # use list comprehension for create dict
 print('Your dictionary is:\n', count)
+
