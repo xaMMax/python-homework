c5f578d4a85ed95e0e5753772745ffa222b833c1
Maks <36572876+xaMMax@users.noreply.github.com>
2021-04-28 22:14:40 +0300

    Update lesson7_task2.py

--- lesson7/lesson7_task2.py
+++ lesson7/lesson7_task2.py
@@ -1,7 +1,6 @@
 def make_country(name, capital):  # define the function
     country_dict = {name.title(): capital.title()}  # create dictionary with defined variables
-    print(country_dict)  # print our result
-    return  # return function
+    return counry_dict # return function
 
 
 name = input('Please input the name of the country: ')  # use prompt for input country name
