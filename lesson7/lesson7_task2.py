62ce1ec4d37df0328c28cadb4db109fb796c901e
xaMMax <petrolijuice@gmail.com>
2021-04-29 18:32:26 +0300

    Lesson 7 bugFix

--- lesson7/lesson7_task2.py
+++ lesson7/lesson7_task2.py
@@ -1,9 +1,9 @@
 def make_country(name, capital):  # define the function
     country_dict = {name.title(): capital.title()}  # create dictionary with defined variables
-    return counry_dict # return function
+    return country_dict  # return function
 
 
 name = input('Please input the name of the country: ')  # use prompt for input country name
 capital = input('Please input the capital of this country: ')  # use prompt for input country name
 
-make_country(name, capital)  # call function
+print(make_country(name, capital))  # call function
--- lesson7/lesson7_task3.py
+++ lesson7/lesson7_task3.py
@@ -1,20 +1,28 @@
+from operator import  sub
+from functools import reduce
+
+
 def make_operation(x='', *args):  # define the function called make_operation
     result = 1  # use variable for use in operatoins
+    # multiplication
     if x == '*':  # if else statement for choice the type of operation
         for i in args:
             result *= i
-            print(result)
+        print(result)
+    # addition
     elif x == '+':
         result = sum(args)
         print(result)
+    # subtraction
     elif x == '-':
-        result = args[0] - args[1]
-        print('Sorry at this time you can subtract only two arguments', args[0], 'and', args[1],'resultis',  result)
+        args = list(args)
+        result = reduce(sub, args)
+        print(result)
+    # if x not equal '*', '+', '-'
     else:
-        print('it is bad func')
+        print('it is bad idea')
+    return   # return result of operations
 
-    return  # return result of operations
 
-
-make_operation('+', 5, 6, 2)  # call the function
+make_operation('-', 5, 6, 2)  # call the function
 
