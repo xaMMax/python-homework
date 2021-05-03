737381bb82a66627f0e8d419b44df1fc7a985240
xaMMax <petrolijuice@gmail.com>
2021-05-03 17:27:00 +0300

    Lesson 8

--- lesson8/lesson8_task2.py
+++ lesson8/lesson8_task2.py
@@ -1,19 +1,14 @@
-def calculator():
-    a = input('Input first number\na= ')
-    b = input('Input second number\nb= ')
-    try:
-        a.isdigit()
-        b.isdigit()
-        a = int(a)
-        b = int(b)
-        result = (a*a) / b
-        print('Result of (a power 2) divided by b is', result)
-    except ValueError:
-        print('Value error, use digits')
-        calculator()
-    except ZeroDivisionError:
-        print('ZeroDivision Error, you can`t divide by zero')
-        calculator()
+def calculator():  # call the function
+    try:  # try except construction
+        a = int(input('Input first number\na= '))  # user input number called a
+        b = int(input('Input second number\nb= '))  # user input number called b
+        print('Result of (a power 2) divided by b is', (a*a) / b)  # print result if our user correctly input variables
+    except ValueError:  # use value error exception for pull out all non digits elements
+        print('Value error, use digits')  # print type of except for user
+        calculator()  # recall function for repeat users input
+    except ZeroDivisionError:  # use zero division error exception because we can not divide by zero
+        print('ZeroDivision Error, you can`t divide by zero')  # print type of except for user
+        calculator()  # recall function for repeat users input
     return
 
 
