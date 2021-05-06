b18443ab65e9b4896ae13b1345dac376f057f5dd
xaMMax <petrolijuice@gmail.com>
2021-05-06 09:39:28 +0300

    Lesson 9 TASK 1

--- lesson9/lesson9_task1_create.py
+++ lesson9/lesson9_task1_create.py
@@ -1,5 +1,3 @@
-
-string0 = ('Hello file world!\n')
-file_path = '/home/maks/PycharmProjects/python-homework/lesson9/myfile.txt'
-with open(file_path, 'w') as file_object:
-    file_object.write(string0)
+myfile = open('/home/maks/PycharmProjects/python-homework/lesson9/myfile.txt', 'w')
+myfile.write('Hello file world!')
+myfile.close()
--- lesson9/lesson9_task1_read.py
+++ lesson9/lesson9_task1_read.py
@@ -1,5 +1,2 @@
-file_path = '/home/maks/PycharmProjects/python-homework/lesson9/myfile.txt'
-
-with open(file_path) as file_object:
-    string0 = file_object.read()
-print(string0)
+myfile = open('/home/maks/PycharmProjects/python-homework/lesson9/myfile.txt')
+print(myfile.read())
