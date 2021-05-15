cb9aaa8476ccb5c8b6bdf1e60c31651bad304fe7
Maks <36572876+xaMMax@users.noreply.github.com>
2021-05-15 20:12:28 +0300

    Delete lesson6 directory

--- lesson6/lesson6_task1.py
+++
@@ -1,5 +0,0 @@
-m = input('Input text here: ')  # create string
-m = m.split()  # use split for crate some list
-count = {i: m.count(i) for i in m if i != ''}  # use list comprehension for create dict
-print('Your dictionary is:\n', count)
-
