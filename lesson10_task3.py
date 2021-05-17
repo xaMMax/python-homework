935775da8aaa0d9c69a8d0e4c5c752dd70353a0a
xaMMax <petrolijuice@gmail.com>
2021-05-17 17:51:55 +0300

    lesson 10

--- lesson10_task3.py
+++ lesson10_task3.py
@@ -2,25 +2,24 @@ channels = ['BBC', 'Discovery', 'TV1000']
 
 class TVController():
 
-    def __init__(self, channels):
+    def __init__(self):
         self.channels = channels
 
     def first_channel(self):
-        channel = channels[0]
-        print(channel)
+        print(self.channels[0])
+        return self.channels[0]
 
     def last_channel(self):
-        channel = channels[-1]
-        print(channel)
+        print(self.channels[-1])
+        return self.channels
 
     def turn_channel(self, n):
-        channel = channels[n-1]
-        print(channel)
-        return channel
+        print(self.channels[n-1])
+        return self.channels
 
     def next_channel(self):
-        channel = channels
-        print(channel[1])
+        print(self.channels[+1])
+        return self.channels
 
     def previous_channel(self):
         print(channels[0])
@@ -30,18 +29,21 @@ class TVController():
 
     def is_exist(self, a):
         if a in channels:
-            print(channels.index(a), a)
+            print(a)
         else:
             print('No')
 
-new_control = TVController(channels)
+    def __str__(self):
+        return self.channels
+
+new_control = TVController()
 #
-# new_control.first_channel()
-# new_control.last_channel()
-# new_control.turn_channel(1)
-# new_control.next_channel()
-# new_control.previous_channel()
-# new_control.current_channel()
-new_control.is_exist('BBC')
+new_control.first_channel()
+new_control.last_channel()
+new_control.turn_channel(2)
+new_control.next_channel()
+new_control.previous_channel()
+new_control.current_channel()
+new_control.is_exist('Discovery')
 new_control.is_exist(4)
 
