0dcdb3cde4b424aa1df2d90cb5f2d792d2d3a6b9
xaMMax <petrolijuice@gmail.com>
2021-05-24 17:18:34 +0300

    lesson 15 task 1 моя вродь поправил

--- lesoon15_task1.py
+++ lesoon15_task1.py
@@ -20,16 +20,6 @@ class EmailValid:
     def __init__(self, email):
         self.email = email
 
-    # def validate(*args):
-    #     pattern = compile('(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
-    #     address = input('inter you email address:')
-    #     is_valid = pattern.match(address)
-    #     if is_valid:
-    #         print('правильный email:', is_valid.group())
-    #             # объект is_valid содержит 3 метода
-    #         # print('методы: start:', is_valid.start(), 'end:', is_valid.end(), 'group:', is_valid.group())
-    #     else:
-    #         print('неверный email! введите email...\n')
 
 newmail = EmailValid('asfsdf@gmail.com')
 
