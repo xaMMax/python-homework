95140f4a9f0a93a6115471c18f6a026053bb3269
xaMMax <petrolijuice@gmail.com>
2021-05-25 18:42:32 +0300

    lesson 15 task 3, мне кажется все равно что то не правильно так как оно просто смотрит правильные ли типы данных и все.

--- lesson15_task3.py
+++ lesson15_task3.py
@@ -1,9 +1,6 @@
-import re
-
 class TypeDecorators:
     def __init__(self, *args):
         self.args = args
-
     @staticmethod
     def to_int(func):
         def converter(*args, **kwargs):
@@ -14,7 +11,6 @@ class TypeDecorators:
             else:
                 raise ValueError('Yor integer string is not integer!')
         return converter
-
     @staticmethod
     def to_str(func):
         def converter(*args, **kwargs):
@@ -22,8 +18,6 @@ class TypeDecorators:
             x = str(x)
             return func(x)
         return converter
-
-
     @staticmethod
     def to_bool(func):
         def converter(*args, **kwargs):
@@ -32,8 +26,6 @@ class TypeDecorators:
             else:
                 raise ValueError('Yor boolean expression is not boolean!')
         return converter
-
-
     @staticmethod
     def to_float(func):
         def converter(*args, **kwargs):
@@ -44,7 +36,6 @@ class TypeDecorators:
         return converter
 
 
-
 @TypeDecorators.to_float
 # @TypeDecorators.to_bool
 # @TypeDecorators.to_int
@@ -53,26 +44,7 @@ def do_something(string:str):
     return string
 ###################################
 #---------convert all to integer-------
+
+
 x = do_something(1.2)
 print(x)
-# print(type(x))
-# a = x + 3
-# print(a)
-###################################
-#---------convert to string-------
-# x = do_something(21.651)
-# print(x)
-# print(type(x))
-
-
-
-#
-#
-#
-# @TypeDecorators.to_bool
-# def do_nothing(string:str):
-#     return string
-#
-# x = do_nothing(12)
-# print(x)
-
