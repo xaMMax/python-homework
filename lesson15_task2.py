0c2502630ec7f42925358d8b8984a3d819092af0
xaMMax <petrolijuice@gmail.com>
2021-05-25 18:07:21 +0300

    lesson 15 task 2 я наверное не все понял но это вариант получше

--- lesson15_task2.py
+++ lesson15_task2.py
@@ -4,47 +4,45 @@ class Boss:
         self.name = name
         self.company = company
         self.workers = []
-        pass
-    def add_workers(self, *args):
-        self.workers.append(args)
-        return self.workers
-
+    def add_workers(self, workr):
+        if isinstance(workr, Worker):
+            self.workers.append(workr)
+            return self.workers
+        else:
+            print(f'you cant add non workers object {workr}')
     def __repr__(self):
         return f'The {self.name}'
 
+
 class Worker:
-    def __init__(self, id_:int, name:str, company:str, __boss: Boss):
+    def __init__(self, id_:int, name:str, company:str, boss_is=Boss):
         self.id = id_
         self.name = name
         self.company = company
-        self.__boss = __boss
-        pass
+        self.__boss = boss_is
     @property
     def boss(self):
         return self.__boss
     @boss.setter
     def boss(self, boss_is):
-        self.__boss = boss_is
-        return
-
-
+        if isinstance(boss_is, Boss):
+            self.__boss = boss_is
+        else:
+            print(f'you cant add non boss object')
     def __repr__(self):
         return f'Worker {self.name}, with id #{self.id}, worked in {self.company} boss is {self.__boss}'
 
 
 new_boss = Boss(1, 'Max', 'IT~LSD')
-worker1 = Worker(11, 'Vasya', 'IT~LSD', new_boss.name)
-worker2 = Worker(12,'Nigga', 'IT~LSD', new_boss.name)
-worker3 = Worker(13,'Chiny', 'It~LSD', new_boss.name)
+worker1 = Worker(11, 'Vasya', 'IT~LSD')
+worker2 = Worker(12,'Nigga', 'IT~LSD')
+worker3 = Worker(13,'Chiny', 'It~LSD')
 
+
+worker1.__boss = new_boss
 new_boss.add_workers(worker1)
-new_boss.add_workers(worker2)
-new_boss.add_workers(worker3)
 
-worker1.__boss = 'Some Vasiliy'
-worker2.__boss = new_boss.name
 
 print(worker1.__boss)
-print(worker2.__boss)
-print(worker3)
-
+print(new_boss.workers)
+print(new_boss)
