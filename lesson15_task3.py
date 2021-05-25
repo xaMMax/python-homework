6b0dd8cadb9d708f510f5143531772460a797726
xaMMax <petrolijuice@gmail.com>
2021-05-25 18:40:28 +0300

    lesson 15 task 3, мне кажется все равно не правильно

--- lesson15_task3.py
+++ lesson15_task3.py
@@ -6,15 +6,14 @@ class TypeDecorators:
 
     @staticmethod
     def to_int(func):
-            def converter(*args, **kwargs):
-                x = (args[0])
-                try:
-                    x = int(re.sub('\D','', x))
-                    return func(x)
-                except ValueError:
-                    return f'Yor integer string is not integer! {func}'
-            return converter
-
+        def converter(*args, **kwargs):
+            x = (args[0])
+            if x.isnumeric():
+                print(int(x))
+                return func(x)
+            else:
+                raise ValueError('Yor integer string is not integer!')
+        return converter
 
     @staticmethod
     def to_str(func):
@@ -28,37 +27,34 @@ class TypeDecorators:
     @staticmethod
     def to_bool(func):
         def converter(*args, **kwargs):
-            try:
-                bool(args)
-                return func(args)
-            except ValueError:
-                return f'Yor boolean string is not boolean! {func}'
+            if type(args[0]) == bool:
+                return func(args[0])
+            else:
+                raise ValueError('Yor boolean expression is not boolean!')
         return converter
 
 
     @staticmethod
     def to_float(func):
         def converter(*args, **kwargs):
-            x = (args[0])
-            try:
-                x = re.findall('\d+\.\d+', x)
-                return func(x)
-            except ValueError:
-                return f'Yor integer string is not integer! {func}'
+            if type(args[0]) == float:
+                return func(args[0])
+            else:
+                raise ValueError('Yor float number is not float!')
         return converter
 
 
 
-
-
+@TypeDecorators.to_float
+# @TypeDecorators.to_bool
 # @TypeDecorators.to_int
-@TypeDecorators.to_str
+# @TypeDecorators.to_str
 def do_something(string:str):
     return string
 ###################################
 #---------convert all to integer-------
-# x = do_something('3, 3')
-# print(x)
+x = do_something(1.2)
+print(x)
 # print(type(x))
 # a = x + 3
 # print(a)
@@ -70,13 +66,13 @@ def do_something(string:str):
 
 
 
-
-
-
-@TypeDecorators.to_bool
-def do_nothing(string:str):
-    return string
-
-x = do_nothing(12)
-print(x)
+#
+#
+#
+# @TypeDecorators.to_bool
+# def do_nothing(string:str):
+#     return string
+#
+# x = do_nothing(12)
+# print(x)
 
