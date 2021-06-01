f0a74241e53b29b9fdc6360d579fe1e48ff605c2
xaMMax <petrolijuice@gmail.com>
2021-06-01 13:47:19 +0300

    lesson 16 task2 беллисимо как по мне)))

--- lesson16_task2.py
+++ lesson16_task2.py
@@ -1,20 +1,26 @@
 class Iterator:
-    def __init__(self, start, end, step=1):
-        self.start = start
-        self.end = end
-        self.step = step
+    def __init__(self, start=0, end=None, step=1):
+        if end == None:
+            end = start
+            self.start = 0
+            self.end = end
+            self.step = step
+        else:
+            self.start = start
+            self.end = end
+            self.step = step
 
     def __iter__(self):
         return self
     def __next__(self):
-        if self.start > self.end:
+        return_value = self.start
+        if self.start < self.end:
+            self.start += self.step
+        else:
             raise StopIteration
-        self.start += self.step
-        return self.start
+        return return_value
 
 
 in_range = Iterator
-in_range = in_range(0,20,5)
-
-for x in in_range:
+for x in in_range(2, 20, 2):
     print(x)
