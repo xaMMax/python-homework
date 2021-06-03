10706fcf58d1e79d1faa49dfd47ab10534721a5d
xaMMax <petrolijuice@gmail.com>
2021-06-03 12:41:34 +0300

    lesson 18 task1

---
+++ lesson18_tack1_catalog/importing_function.py
@@ -0,0 +1,8 @@
+from lesson18_tack1_catalog.some_function import this_some_func_for_import
+
+
+def string_sumator():
+    return f'string sumator + {this_some_func_for_import()}'
+
+
+print(string_sumator())
---
+++ lesson18_tack1_catalog/some_function.py
@@ -0,0 +1,2 @@
+def this_some_func_for_import():
+    return('function from lib lesson18_tack1_catalog, to module importing_function')
