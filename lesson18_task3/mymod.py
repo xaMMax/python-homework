c19c14bfcb96d25cdb90c8b4e56c6326c56e7b54
xaMMax <petrolijuice@gmail.com>
2021-06-03 18:32:04 +0300

    lesson 18 task 3

---
+++ lesson18_task3/mymod.py
@@ -0,0 +1,39 @@
+import sys
+
+
+import os, fnmatch
+def find(*args, path = '/home'):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, *args):
+                result.append(os.path.join(root, name))
+    return result
+
+
+def count_lines(name):
+        f = open(name, 'r')
+        lines = f.readlines()
+        return len(lines)
+
+
+def count_chars(name):
+        f = open(name, 'r')
+        result = f.read()
+        return len(result)
+
+
+def test(name):
+    print(f'Your file has\n{count_lines(name)} lines,\n{count_chars(name)} chars')
+
+
+try:
+     test((find(sys.argv[1])[0]))
+except IndexError:
+    try:
+        name = input('Input filename without PATH, i`m find file automatically ')
+        test((find(name)[0]))
+    except IndexError:
+            exit('I can`t find your file, check all and try again.')
+
+
