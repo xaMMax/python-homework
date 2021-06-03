02fd2c0f0318eaef2a45afa5a251cbf3dcc340b6
xaMMax <petrolijuice@gmail.com>
2021-06-03 19:01:34 +0300

    lesson 18 task 3

--- lesson18_task3/mymod.py
+++ lesson18_task3/mymod.py
@@ -1,7 +1,5 @@
-import sys
+import sys, os, fnmatch
 
-
-import os, fnmatch
 def find(*args, path = '/home'):
     result = []
     for root, dirs, files in os.walk(path):
@@ -11,19 +9,19 @@ def find(*args, path = '/home'):
     return result
 
 
-def count_lines(name):
+def count_lines(name: str):
         f = open(name, 'r')
         lines = f.readlines()
         return len(lines)
 
 
-def count_chars(name):
+def count_chars(name: str):
         f = open(name, 'r')
         result = f.read()
         return len(result)
 
 
-def test(name):
+def test(name: str):
     print(f'Your file has\n{count_lines(name)} lines,\n{count_chars(name)} chars')
 
 
