d8cb264aac4411539f3985483eac93dffa0efc8c
xaMMax <petrolijuice@gmail.com>
2021-07-15 14:11:56 +0300

    lesson24 t2

--- lesson24/lesson24_task2.py
+++ lesson24/lesson24_task2.py
@@ -5,12 +5,16 @@ from pythonds.basic.stack import Stack
 
 def del_symbols(symbol_string):
     symbol_string = re.sub('[A-Za-z0-9]', '', symbol_string)
-    return parChecker(symbol_string)
+    return par_checker(symbol_string)
 
 
-def parChecker(symbol_string):
+def matches(opn, close):
+    opens = "({["
+    closes = ")}]"
+    return opens.index(opn) == closes.index(close)
 
 
+def par_checker(symbol_string):
     s = Stack()
     balanced = True
     index = 0
@@ -22,18 +26,21 @@ def parChecker(symbol_string):
             if s.isEmpty():
                 balanced = False
             else:
-                s.pop()
-
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
         index = index + 1
-
     if balanced and s.isEmpty():
         return True, symbol_string
     else:
         return False, symbol_string
 
-print(del_symbols('(sdfs((sdfasdf)sdfas))'))
-print(del_symbols('((asdf)'))
-print(del_symbols('[[sdfsad]]'))
-print(del_symbols('[[sdfas]'))
-print(del_symbols('{{sdfs}}'))
-print(del_symbols('{{sdfs}'))
+
+if __name__ == '__main__':
+    print(del_symbols('(sdfs((sdfasdf)sdfas))'))
+    print(del_symbols('((asdf)'))
+    print(del_symbols('[[sdfsad]]'))
+    print(del_symbols('[[sdfas]'))
+    print(del_symbols('{{sdfs}}'))
+    print(del_symbols('{{sdfs}'))
+    print((del_symbols("(((}}}")))
